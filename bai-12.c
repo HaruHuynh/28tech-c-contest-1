fd250fdcc72d9308d490b341372238c0446112fa
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-08 15:33:44 +0700

    Bài 12

---
+++ bai-12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main () {
+    int t,n;
+    scanf("%d%d", &t, &n);
+
+    if(t > 0 && t <= 12 && n > 0) {
+        if(t == 1 || t == 3 || t == 5 || t == 7 || t == 8 || t == 10 || t == 12) {
+            printf("31\n");
+        } else if (t == 4 || t == 6 || t == 9 || t == 11) {
+            printf("30\n");
+        } else {
+            if((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
+                printf("29\n");
+            }else {
+                printf("28\n");
+            }
+        }
+    }else{
+        printf("INVALID\n");
+    }
+    return 0;
+}
