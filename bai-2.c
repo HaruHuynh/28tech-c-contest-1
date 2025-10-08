0a33e1083ac40fcfba90baed551d9cd49f845594
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-08 12:20:22 +0700

    Bài 2

---
+++ bai-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b, c;    
+    scanf("%d %d %d", &a, &b, &c);
+    long long res = 1ll * a * (b + c) + 1ll * b * (a + c);
+    printf("%lld\n", res);
+}
