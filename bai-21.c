ea6440342da0f65f34871a42f3649f437da90fee
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-09 16:25:36 +0700

    Sửa bài 21

--- bai-21.c
+++ bai-21.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 int main () {
-    long long a,b,k;
-    scanf("%lld %lld %lld", &a,&b,&k);
-    long long p = 1ll * (k+1)/2;
-    long long t = 1ll * k/2;
-    long long v = 1ll * (p * a) - (t * b);
-    printf("%lld", v);
+    // long long a,b,k;
+    // scanf("%lld %lld %lld", &a,&b,&k);
+    // long long p = 1ll * (k+1)/2;
+    // long long t = 1ll * k/2;
+    // long long v = 1ll * (p * a) - (t * b);
+    // printf("%lld", v);
+
+    int a, b, k;
+    scanf("%d%d%d", &a,&b,&k);
+    int trai, phai;
+    if (k % 2 == 0){
+        trai = phai = k / 2;
+    }else {
+        trai = k / 2;
+        phai = trai + 1;
+    }
+    printf("%lld", 1ll * phai * a - trai * b);
+    return 0;
 }
 
