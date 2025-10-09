8e9f7609ff8c14f210f8d4f42ff9a0140bb18c08
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-09 16:40:19 +0700

    Sửa bài 22

--- bai-22.c
+++ bai-22.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 int main() {
+    // long long n,s;
+    // scanf("%lld %lld", &n, &s);
+    // long long coin = s / n;
+    // if (s % n != 0) {
+    //     coin = coin + 1;
+    // }   
+    // printf("%lld", coin);
+
     long long n,s;
     scanf("%lld %lld", &n, &s);
-    long long coin = s / n;
-    if (s % n != 0) {
-        coin = coin + 1;
+    if (s % n == 0){
+        printf("%lld", s/n);
+    }else{
+        printf("%lld", s/n+1);
     }
-    printf("%lld", coin);
+    return 0;
 }
